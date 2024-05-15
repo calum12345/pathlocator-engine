c43e4c390ba558e59dc6c7a137c805175a0d4159
calummelrose <121878030+calum12345@users.noreply.github.com>
2024-05-15 12:14:30 +0400

    Updated demo
    Made it a bit more complex to test what would happen if path were deeper

--- path.py
+++ path.py
@@ -3,5 +3,9 @@ from binarytree import Tree
 start = Tree("hi there :). this is a test thing. welcome to the test thing. you can go outside or you can not go outside. enter '1' for the former, and '2' for the latter.", False)
 outside = Tree("you win", True)
 start.option1 = outside
-inside = Tree("you died :(", True)
+inside = Tree("you died :(", False)
 start.option2 = inside
+iLikeThatNumber = Tree("cool number, bro. nice choice", True)
+ew = Tree("you are still dead", True)
+inside.option1 = ew
+inside.option2 = iLikeThatNumber
